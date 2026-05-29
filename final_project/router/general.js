4448c269d1dcb39628800a7bbeaed3c010ea5e7e
hmalq <hmalq2005@icloud.com>
2026-05-29 19:05:33 -0400

    Fixed Promises implementation for Tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Register a new user
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
+
   if (username && password) {
       let doesExist = users.filter((user) => user.username === username);
       if (doesExist.length > 0) {
@@ -52,11 +54,17 @@ public_users.get('/author/:author', function (req, res) {
         const targetAuthor = req.params.author;
         const matchingBooks = [];
         const isbns = Object.keys(books);
+        
         isbns.forEach((isbn) => {
             if (books[isbn].author === targetAuthor) {
-                matchingBooks.push({"isbn": isbn, "title": books[isbn].title, "reviews": books[isbn].reviews});
+                matchingBooks.push({
+                    "isbn": isbn, 
+                    "title": books[isbn].title, 
+                    "reviews": books[isbn].reviews
+                });
             }
         });
+        
         if (matchingBooks.length > 0) {
             resolve(matchingBooks);
         } else {
@@ -76,11 +84,17 @@ public_users.get('/title/:title', function (req, res) {
         const targetTitle = req.params.title;
         const matchingBooks = [];
         const isbns = Object.keys(books);
+        
         isbns.forEach((isbn) => {
             if (books[isbn].title === targetTitle) {
-                matchingBooks.push({"isbn": isbn, "author": books[isbn].author, "reviews": books[isbn].reviews});
+                matchingBooks.push({
+                    "isbn": isbn, 
+                    "author": books[isbn].author, 
+                    "reviews": books[isbn].reviews
+                });
             }
         });
+        
         if (matchingBooks.length > 0) {
             resolve(matchingBooks);
         } else {
